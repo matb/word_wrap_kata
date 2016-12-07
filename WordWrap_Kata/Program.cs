150374fc42ee25a622287a24972a4cc5dd78c9b7
Matthias Baumann <matthias@divisionx.ch>
2016-12-07 16:57:55 +0100

    Add project files.

---
+++ WordWrap_Kata/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordWrap_Kata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
